795e147d5eea88694d3a8c86c333818a57259506
Rafael.Gutierrez <rgutierreztejada@gmail.com>
2019-06-21 13:55:28 -0600

    ajustes

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -23,15 +23,13 @@ const Welcome = () => {
       					<hr/>
       					<Article />
       					
-                                    <nav className="flexbox mb-50">
-                                      <a className="btn btn-white disabled">
-                                        <i className="ti-arrow-left fs-9 mr-4" /> Newer</a>
-                                      <a className="btn btn-white" href="#">Older
-                                        <i className="ti-arrow-right fs-9 ml-4" />
-                                      </a>
-                                    </nav>
-
-
+                            <nav className="flexbox mb-50">
+                                <a className="btn btn-white disabled">
+                                    <i className="ti-arrow-left fs-9 mr-4" /> Newer</a>
+                                <a className="btn btn-white" href="#">Older
+                                    <i className="ti-arrow-right fs-9 ml-4" />
+                                </a>
+                            </nav>
       				</div>
       			</div>
 	  		</main>
